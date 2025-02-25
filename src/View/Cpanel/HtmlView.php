c903a05032e88bd0854e47cf4acca0e99e01b406
Johan Anquetil <114992907+JohanAnquetil@users.noreply.github.com>
2025-02-25 15:13:27 +0100

    Ajout d'un tableau de bord pour la gestion des élus, incluant des sections pour les élus, syndicats et commissions, ainsi qu'un tutoriel d'utilisation.

---
+++ src/View/Cpanel/HtmlView.php
@@ -0,0 +1,14 @@
+<?php
+namespace Joomla\Component\Elus\Administrator\View\Cpanel;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+class HtmlView extends BaseHtmlView
+{
+    public function display($tpl = null)
+    {
+        parent::display($tpl);
+    }
+}
